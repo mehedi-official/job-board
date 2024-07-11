0871b54f3d20a970ab5791702b83c6b4b69c06d5
mehedi-official <74343026+mehedi-official@users.noreply.github.com>
2024-07-11 18:13:24 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: {
+          50: '#666666',
+          100: '#323232',
+          200: '#000000'
+        },
+        silver: {
+          50: 'rgba(255, 255, 255 , 0.6)',
+          100: 'rgba(255, 255, 255, 0.9)',
+          200: '#ffffff'
+        },
+        primary: '#88F32B'
+      }
+    },
+  },
+  plugins: []
+};
